7c7680f1ed51bea3d050eed80e12d7549c974cf1
Jimmy Emery <jimmy.e@outlook.com>
2022-03-22 13:35:24 -0400

    updating manager head

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-postcss',
+  ],
 }
