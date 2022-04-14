a73693c203c97546ed6e7a7c868eef49d081016f
Jimmy Emery <jimmy.e@outlook.com>
2022-04-14 16:06:49 -0400

    updating story

--- stories/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Meta, Story } from '@storybook/react/types-6-0';
-import { Button as ButtonComponent, ButtonProps } from './Button';
-
-export default {
-  component: ButtonComponent,
-} as Meta;
-
-export const Button: Story<ButtonProps> = (args) => <ButtonComponent {...args} />;
